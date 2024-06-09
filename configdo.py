1492253830a6e2d28513c21d01a24efc7b0fc85e
David Soh <ln@trth.nl>
2024-06-09 23:59:53 +0800

    fix: configdo error

--- configdo.py
+++ configdo.py
@@ -5,17 +5,17 @@ import rtoml
 class ConfigCla:
     """Configuration class"""
     def __init__(self,input_str:str):
-        self.prefix = input_str
+        self.prefix = f"record/{input_str}"
         self.dict = dict()
-        self.dict.update(dict(rtoml.load(open(input_str+"/config.toml",encoding="utf8"))))
+        self.dict.update(dict(rtoml.load(open(f"record/{input_str}/config.toml",encoding="utf8"))))
         self.rss = self.dict["rss"]
         self.apple = self.dict.get("apple","")
         self.google = self.dict.get("google","")
         self.spotify = self.dict.get("spotify","")
         self.youtube = self.dict.get("youtube","")
         self.correct = self.dict.get("correct",dict())
-        pathlib.Path(input_str+"/record/").mkdir(parents=True,exist_ok=True)
-        pathlib.Path(input_str+"/mid/").mkdir(parents=True,exist_ok=True)
+        pathlib.Path(f"record/{input_str}/record/").mkdir(parents=True,exist_ok=True)
+        pathlib.Path(f"record/{input_str}/mid/").mkdir(parents=True,exist_ok=True)
     def xmlw(self,content_str:str,part_str:str):
         """Export as plain text"""
         with open(self.prefix+part_str,"w",encoding="utf8") as target_handler:
