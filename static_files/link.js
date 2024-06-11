23735cc667098fc6af135846d97c7e3d7994263b
David Soh <ln@trth.nl>
2024-06-11 14:40:50 +0000

    refactor: use KLOLT instead of CFP2
    Signed-off-by: David Soh <ln@trth.nl>

--- static_files/link.js
+++ static_files/link.js
@@ -1,12 +1,12 @@
 var reverseShortDict = {
-"CFP2ASC": {
-"en": ["ASC","CFP2ASC","SST","CFP2SST"],
+"KLOLTASC": {
+"en":   ["ASC","KLOLTASC","CFP2ASC","SST","CFP2SST"],
 "hant": [
 "%E7%A5%9E%E4%BB%99%E8%A3%9C%E7%BF%92%E7%8F%AD"
 ]
 },
 "KLOLTBLG": {
-"en": ["BLG","BAILINGGUO","KLOLTBLG","CFP2BLG","PSB","PSBLG"],
+"en":   ["BLG","BAILINGGUO","KLOLTBLG","CFP2BLG","PSB","PSBLG"],
 "hant": [
 "%E7%99%BE%E9%9D%88%E6%9E%9C",
 "%E7%99%BE%E6%92%AD",
@@ -14,8 +14,8 @@ var reverseShortDict = {
 "%F0%9F%91%8C"
 ]
 },
-"CFP2EXR": {
-"en": ["EXR","EYE","EYEXRADIO","CFP2EXR"],
+"KLOLTEXR": {
+"en":   ["EXR","KLOLTEXR","EYE","EYEXRADIO","CFP2EXR"],
 "hant": [
 "%E7%9C%BC%E7%90%83",
 "%0A%E7%9C%BC%E7%90%83%E5%9C%B0%E4%B8%8B%E9%9B%BB%E5%8F%B0",
@@ -23,63 +23,62 @@ var reverseShortDict = {
 "%0A%F0%9F%91%81%EF%B8%8F"
 ]
 },
-"CFP2JST": {
-"en": ["JST","CFP2JST"],
+"KLOLTJST": {
+"en":   ["JST","KLOLTJST","CFP2JST"],
 "hant": [
 "%E5%94%90%E9%99%BD%E9%9B%9E%E9%85%92%E5%B1%8B"
 ]
 },
 "ME": {
-"en": [
-"ME","ABOUT","ABOUT_ME"],
+"en":   ["ME","ABOUT","ABOUT_ME"],
 "hant": [
 "%E9%97%9C%E6%96%BC",
 "%E9%97%9C%E6%96%BC%E6%88%91"
 ]
 },
 "PEAZIP": {
-"en": ["PEAZIP","PEA","PZ"],
+"en":   ["PEAZIP","PEA","PZ"],
 "hant": ["PEAZIP","PEA","PZ"]
 },
 "WHITE": {
-"en": ["WHITE"],
+"en":   ["WHITE"],
 "hant": ["WHITE"]
 }
 }
 var jumpDict = {
-"CFP2ASC": {
-"en": "Cuttle Fish Podcast Player for \u300a\u795e\u4ed9\u88dc\u7fd2\u73ed\u300b",
+"KLOLTASC": {
+"en":   "klo.lt Podcast Player for \u300a\u795e\u4ed9\u88dc\u7fd2\u73ed\u300b",
 "hant": "\u975e\u5b98\u65b9\u300a\u795e\u4ed9\u88dc\u7fd2\u73ed\u300b\u64ad\u653e\u5ba4",
-"url": "https://xn--158h.xn--2os22eixx6na.xn--kpry57d/"
+"url":  "https://xn--158h.xn--2os22eixx6na.xn--kpry57d/"
 },
 "KLOLTBLG": {
-"en": "klo.lt Podcast Player for Bailingguo",
+"en":   "klo.lt Podcast Player for Bailingguo",
 "hant": "\u975e\u5b98\u65b9\u300a\u767e\u9748\u679c\u300b\u64ad\u653e\u5ba4",
-"url": "https://klo.lt/blg/"
+"url":  "https://klo.lt/blg/"
 },
-"CFP2EXR": {
-"en": "Cuttle Fish Podcast Player for Eye X Radio",
+"KLOLTEXR": {
+"en":   "klo.lt Podcast Player for Eye X Radio",
 "hant": "\u300a\u773c\u7403\u5730\u4e0b\u96fb\u53f0\u300b\u64ad\u653e\u5ba4",
-"url": "https://xn--mp8h.xn--2os22eixx6na.xn--kpry57d/"
+"url":  "https://xn--mp8h.xn--2os22eixx6na.xn--kpry57d/"
 },
-"CFP2JST": {
-"en": "Cuttle Fish Podcast Player for \u300a\u5510\u967d\u96de\u9152\u5c4b\u300b",
+"KLOLTJST": {
+"en":   "klo.lt Podcast Player for \u300a\u5510\u967d\u96de\u9152\u5c4b\u300b",
 "hant": "\u975e\u5b98\u65b9\u300a\u5510\u967d\u96de\u9152\u5c4b\u300b\u64ad\u653e\u5ba4",
-"url": "https://xn--r09h.xn--2os22eixx6na.xn--kpry57d/"
+"url":  "https://xn--r09h.xn--2os22eixx6na.xn--kpry57d/"
 },
 "ME": {
-"en": "My homepage (DJ Tratoh)",
+"en":   "My homepage (DJ Tratoh)",
 "hant": "\u6df1\u6d77\u58a8\u5ba2\u7684\u58a8\u7389\u7a0b\u5e02",
-"url": "https://xn--2os22eixx6na.xn--kpry57d/"
+"url":  "https://xn--2os22eixx6na.xn--kpry57d/"
 },
 "PEAZIP": {
-"en": "PeaZip - Windows x64 - Download Page",
+"en":   "PeaZip - Windows x64 - Download Page",
 "hant": "PeaZip - Windows x64 - Download Page",
-"url": "https://peazip.github.io/peazip-64bit.html"
+"url":  "https://peazip.github.io/peazip-64bit.html"
 },
 "WHITE": {
-"en": "White - Solid Backgrounds",
+"en":   "White - Solid Backgrounds",
 "hant": "\u5168\u767d\u80cc\u666f\u5716",
-"url": "https://www.solidbackgrounds.com/white-solid-color-background.html"
+"url":  "https://www.solidbackgrounds.com/white-solid-color-background.html"
 }
 }
