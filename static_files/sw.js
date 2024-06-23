90ff2367ef8bcb40bc4aaac96363be3d37ebe4be
David Soh <ln@trth.nl>
2024-06-23 17:52:37 +0800

    mark: rev000015

--- static_files/sw.js
+++ static_files/sw.js
@@ -1,5 +1,5 @@
 // reference: https://github.com/mdn/sw-test
-const currentVersion = 'rev000014';
+const currentVersion = 'rev000015';
 const addResourcesToCache = async (resources) => {
     const cache = await caches.open(currentVersion);
     await cache.addAll(resources);
