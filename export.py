705357a2c3ba165bcb38f778191e6e4111af0cd5
David Soh <ln@trth.nl>
2024-06-28 21:59:28 +0800

    fix: klt naming error

--- export.py
+++ export.py
@@ -8,6 +8,10 @@ import configdo
 
 def convert_month(input_str):
     """get month tag"""
+    return int(datetime.strptime(input_str,"%b %Y").strftime("%Y%m"))
+
+def convert_date(input_str):
+    """get month tag"""
     return int(datetime.strptime(input_str,"%b %d, %Y").strftime("%Y%m%d"))
 
 def do_job(target_str):
@@ -21,6 +25,7 @@ def do_job(target_str):
     keyword_doc = rtoml.load(open(keyword_fn_str,encoding="utf8"))
     date_fn_str = f"record/{target_str}/record/feedPodcast-month.toml"
     date_doc = rtoml.load(open(date_fn_str,encoding="utf8"))
+    dt_list = [m for m in date_doc.values()]
     bY2Y_dict = {datetime.strptime(m,"%b %d, %Y").strftime("%b %Y"):datetime.strptime(m,"%b %d, %Y").strftime("%Y") for m in date_doc.values()}
     rvs_m_dict = {} # reverse_date_dict
     for monthyear_str, year_str in bY2Y_dict.items():
@@ -54,7 +59,7 @@ def do_job(target_str):
         tag_list.extend(sorted(list(set(category_list))))
         deduplicate_tag_list = []
         for tag in tag_list:
-            if tag not in deduplicate_tag_list and tag not in bY2Y_dict.items():
+            if tag not in deduplicate_tag_list and tag not in d2Y_dict.keys():
                 deduplicate_tag_list.append(tag)
         value_inner_dict["tag"] = deduplicate_tag_list
         playlist_dict[key_str] = value_inner_dict
@@ -95,9 +100,9 @@ def do_job(target_str):
     playlist_path = "docs/klt-playlist.json"
     playlist_json = json.load(open(playlist_path)) if Path(playlist_path).exists() else {}
     for key, values in playlist_dict.items():
-        bY_str = [n for n in values["tag"] if n in bY2Y_dict.keys()][0]
-        ym_str = convert_month(bY_str)
-        playlist_json[f"klt{ym_str}_{key}"] = values
+        dt_str = [n for n in values["tag"] if n in dt_list][0]
+        ymd_str = convert_date(dt_str)
+        playlist_json[f"klt{ymd_str}_{key}"] = values
     with open(playlist_path,"w") as target_handler:
         json.dump(playlist_json,target_handler,indent=0,sort_keys=True,ensure_ascii=True)
     print(f"    update docs/klt-tag_class")
