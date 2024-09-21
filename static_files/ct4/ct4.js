0591b645ee730ad43c0092cdcc0ab9e4481fca6f
David Soh <ln@trth.nl>
2024-09-21 04:50:58 +0000

    refactor: adjust podcast info

--- static_files/ct4/ct4.js
+++ static_files/ct4/ct4.js
@@ -1,4 +1,4 @@
-final_title_str="klo.lt - CT4《娛人時代》播放室";
+final_title_str="klo.lt - CT4《娛人的喜劇日常》播放室";
 final_root_path="https://klo.lt/ct4/";
 final_artist_str="娛人時代";
 channel="ct4";
@@ -6,15 +6,15 @@ channel="ct4";
 document.getElementById("aboutA").href="https://github.com/SotongDJ/klo.lt";
 document.getElementById("emailA").href="mailto:ask@klo.lt?subject=（klo.lt-CT4）回饋信件&body=內容類型：（問題？建議？）%0A細節：（越詳細越好）";
 document.getElementById("infoA").href="https://github.com/SotongDJ/klo.lt/wiki";
-document.getElementById("titleCSpan").innerText="娛人時代";
+document.getElementById("titleCSpan").innerText="娛人的喜劇日常";
 document.getElementById("titleEmoji").innerText="🎫";
 document.getElementById("titleESpan").innerText="CT4";
 document.getElementById("titleSec").href="https://instagram.com/comedytimes0401";
-document.getElementById("titleSec").title="娛人時代";
+document.getElementById("titleSec").title="娛人的喜劇日常";
 document.getElementById("sitleEmoji").innerText="🎫";
 document.getElementById("sitleESpan").innerText="CT4";
 document.getElementById("sitleSec").href="https://instagram.com/comedytimes0401";
-document.getElementById("sitleSec").title="娛人時代";
+document.getElementById("sitleSec").title="娛人的喜劇日常";
 
 show_apple=false;
 show_google=false;
