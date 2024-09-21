d1a677e1a1e022cdd378631041f3dec80fd85709
David Soh <ln@trth.nl>
2024-09-21 03:03:35 +0000

    v01r00029: changes - fix agent - disable apple - prepare for more podcast - change frequency - adjust appearance

--- static_files/extra.js
+++ static_files/extra.js
@@ -1,6 +1,6 @@
 if (parameter=="") {
 document.getElementById("content").style['display'] = "none";
-document.getElementById("bound").className = "bound";
+document.getElementById("bound").className = "center";
 document.getElementById("bar").style.display = "none";
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
