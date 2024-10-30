02b62ebbda7cb4da71a24c33189ed7832f3eedba
David Soh <ln@trth.nl>
2024-10-30 07:02:04 +0000

    fix: git commit condition for update_bot

--- klo.lt.sh
+++ klo.lt.sh
@@ -4,115 +4,116 @@ cd /mnt/sd3/record/klo.lt/
 git pull
 rm temp
 pip3 install --user -r requirements.txt
-# Update rss - update 08C
+
+echo Update rss - update 08C
 python3 update.py 08c
 python3 merge.py 08c
 python3 annotate.py 08c > record/08c/filter.txt
 python3 export.py 08c `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update ASC
+echo Update rss - update ASC
 python3 update.py asc
 python3 merge.py asc
 python3 annotate.py asc > record/asc/filter.txt
 python3 export.py asc `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update AYS
+echo Update rss - update AYS
 python3 update.py ays
 python3 merge.py ays
 python3 annotate.py ays > record/ays/filter.txt
 python3 export.py ays `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update BLG
+echo Update rss - update BLG
 python3 update.py blg
 python3 merge.py blg
 python3 annotate.py blg > record/blg/filter.txt
 python3 export.py blg `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update bts
+echo Update rss - update bts
 python3 update.py bts
 python3 merge.py bts
 python3 annotate.py bts > record/bts/filter.txt
 python3 export.py bts `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update ct4
+echo Update rss - update ct4
 python3 update.py ct4
 python3 merge.py ct4
 python3 annotate.py ct4 > record/ct4/filter.txt
 python3 export.py ct4 `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update EXR
+echo Update rss - update EXR
 python3 update.py exr
 python3 merge.py exr
 python3 annotate.py exr > record/exr/filter.txt
 python3 export.py exr `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update fcf
+echo Update rss - update fcf
 python3 update.py fcf
 python3 merge.py fcf
 python3 annotate.py fcf > record/fcf/filter.txt
 python3 export.py fcf `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update fnf
+echo Update rss - update fnf
 python3 update.py fnf
 python3 merge.py fnf
 python3 annotate.py fnf > record/fnf/filter.txt
 python3 export.py fnf `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update hsd
+echo Update rss - update hsd
 python3 update.py hsd
 python3 merge.py hsd
 python3 annotate.py hsd > record/hsd/filter.txt
 python3 export.py hsd `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update inf
+echo Update rss - update inf
 python3 update.py inf
 python3 merge.py inf
 python3 annotate.py inf > record/inf/filter.txt
 python3 export.py inf `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update JST
+echo Update rss - update JST
 python3 update.py jst
 python3 merge.py jst
 python3 annotate.py jst > record/jst/filter.txt
 python3 export.py jst `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update nkw
+echo Update rss - update nkw
 python3 update.py nkw
 python3 merge.py nkw
 python3 annotate.py nkw > record/nkw/filter.txt
 python3 export.py nkw `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update plr
+echo Update rss - update plr
 python3 update.py plr
 python3 merge.py plr
 python3 annotate.py plr > record/plr/filter.txt
 python3 export.py plr `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update pws
+echo Update rss - update pws
 python3 update.py pws
 python3 merge.py pws
 python3 annotate.py pws > record/pws/filter.txt
 python3 export.py pws `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update stp
+echo Update rss - update stp
 python3 update.py stp
 python3 merge.py stp
 python3 annotate.py stp > record/stp/filter.txt
 python3 export.py stp `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update tts
+echo Update rss - update tts
 python3 update.py tts
 python3 merge.py tts
 python3 annotate.py tts > record/tts/filter.txt
 python3 export.py tts `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update twc
+echo Update rss - update twc
 python3 update.py twc
 python3 merge.py twc
 python3 annotate.py twc > record/twc/filter.txt
 python3 export.py twc `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
-# Update rss - update wbr
+echo Update rss - update wbr
 python3 update.py wbr
 python3 merge.py wbr
 python3 annotate.py wbr > record/wbr/filter.txt
@@ -120,7 +121,7 @@ python3 export.py wbr `TZ='<UTC+8>-8' date +'d%y%m%dt%H%M'`
 
 echo `git status --porcelain` > temp
 cat temp
-if [[ "$temp" == "" ]]; then
+if [[ -s temp ]]; then
     git add -A
     git commit -S -m "___ RSS FEED UPDATE "`TZ='<UTC+8>-8' date +'%b_%d,_%Y_%H:%M:%S'`" #bot_action"
     git push
